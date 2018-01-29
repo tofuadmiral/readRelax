e3ba929471fc83e9d2e194fee5222bc5cd3f7737
Ahmed Fuad Ali <fuad98ali@gmail.com>
2018-01-29 18:06:48 -0500

    Front end files written by Brian Chiu

---
+++ graphics.py
@@ -0,0 +1,5 @@
+Python 2.7.14 (default, Sep 25 2017, 09:53:22) 
+[GCC 4.2.1 Compatible Apple LLVM 9.0.0 (clang-900.0.37)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
